380edf01979bf417868b603a795640a709638124
Chad Payne <chad@bird-dog.ca>
2010-08-18 11:49:46 -0600

    Removal of unused regions for widgets. Enabled old menu

--- header.php
+++ header.php
@@ -27,26 +27,9 @@
 
       <?php $dm_page = get_page_by_title('Directory Menu'); ?>
       <div id="menubar" class="grid_12 alpha omega">
-        <div class='nav'>
-          <ul>
           <?php
-            //wp_list_pages('child_of='.$dm_page->ID.'&depth=1&title_li=&sort_column=menu_order');
-            wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-header' ) );
+            wp_list_pages('child_of='.$dm_page->ID.'&depth=1&title_li=&sort_column=menu_order');
+            //wp_nav_menu(array('sort_column' => 'menu_order', 'container_class' => 'nav' ) );
           ?>
-          </ul>
-        </div>
       </div>
-
-      <div class="clear"></div>
-
-      <div id="submenu" class="subnav">
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("submenu") ) : ?><?php endif; ?>
-      </div>
-
-      <div class="clear"></div>
-
-      <div id="top-banner-ad">
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("top-banner-ad") ) : ?><?php endif; ?>
-      </div>
-
       <div class="clear"></div>
